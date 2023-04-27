45da0810bb75a66f9c961bea1d3fead1ea32691a
Blake Bishop <b.bishop3709@student.nu.edu>
2023-04-26 19:54:39 -0700

    Local: committing style changes

--- eTickets/eTickets/Controllers/MoviesController.cs
+++ eTickets/eTickets/Controllers/MoviesController.cs
@@ -16,7 +16,7 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> IndexAsync()
         {
-            var allMoviess = await _context.Movies.ToListAsync();
+            var allMoviess = await _context.Movies.Include(n => n.Cinema).ToListAsync();
             return View(allMoviess);
         }
     }
