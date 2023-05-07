2ca30f84c90e6075c3cb2289802e76f77e1ee89e
Blake Bishop <b.bishop3709@student.nu.edu>
2023-05-06 17:56:03 -0700

    Local: integrated Producers service

--- eTickets/eTickets/Startup.cs
+++ eTickets/eTickets/Startup.cs
@@ -32,6 +32,7 @@ namespace eTickets
 
             // Service Configs
             services.AddScoped<IActorsService, ActorsService>();
+            services.AddScoped<IProducersService, ProducersService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
