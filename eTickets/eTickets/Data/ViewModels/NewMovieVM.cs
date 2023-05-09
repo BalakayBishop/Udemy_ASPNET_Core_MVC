431e8cd35fe747ff163c33f013e654834cfc90b1
Blake Bishop <b.bishop3709@student.nu.edu>
2023-05-09 16:20:32 -0700

    Local: integrated create movie form

--- eTickets/eTickets/Data/ViewModels/NewMovieVM.cs
+++ eTickets/eTickets/Data/ViewModels/NewMovieVM.cs
@@ -11,46 +11,46 @@ namespace eTickets.Models
 {
     public class NewMovieVM
     {
+        [Display(Name = "Movie Name")]
         [Required(ErrorMessage = "Name is required")]
-        [Display(Description = "Movie Name")]
         public string Name { get; set; }
 
+        [Display(Name = "Movie Description")]
         [Required(ErrorMessage = "Description is required")]
-        [Display(Description = "Movie Description")]
         public string Description { get; set; }
 
+        [Display(Name = "Movie Art URL")]
         [Required(ErrorMessage = "Movie cover art URL is required")]
-        [Display(Description = "Movie Cover Art URL")]
         public string ImageURL { get; set; }
-
+        
+        [Display(Name = "Movie Price in $")]
         [Required(ErrorMessage = "Price is required")]
-        [Display(Description = "Movie Price")]
         public double Price { get; set; }
 
+        [Display(Name = "Movie Start Date")]
         [Required(ErrorMessage = "Start date is required")]
-        [Display(Description = "Movie Start Date")]
         public DateTime Start_Date { get; set; }
 
+        [Display(Name = "Movie End Date")]
         [Required(ErrorMessage = "End date is required")]
-        [Display(Description = "Movie End Date")]
         public DateTime End_Date { get; set; }
 
+        [Display(Name = "Movie Category")]
         [Required(ErrorMessage = "Movie category is required")]
-        [Display(Description = "Movie Category")]
         public MovieCategory MovieCategory { get; set; }
 
+        [Display(Name = "Movie actor(s) is required")]
         [Required(ErrorMessage = "Select one or more actor(s)")]
-        [Display(Description = "Movie actor(s) is required")]
         public List<int> Actor_IDs { get; set; }
 
         // Cinema
+        [Display(Name = "Select a Cinema")]
         [Required(ErrorMessage = "Cinema is required")]
-        [Display(Description = "Select a Cinema")]
         public int Cinema_ID { get; set; }
 
         // Producer
+        [Display(Name = "Select a Producer")]
         [Required(ErrorMessage = "Producer is required")]
-        [Display(Description = "Select a Producer")]
         public int Producer_ID { get; set; }
     }
 }
