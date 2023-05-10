6a288083a6b112c51a298674332b18ff934666cf
Blake Bishop <b.bishop3709@student.nu.edu>
2023-05-10 16:40:12 -0700

    Local: Adding Items to shopping cart

--- eTickets/eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/eTickets/Data/Cart/ShoppingCart.cs
@@ -18,6 +18,29 @@ namespace eTickets.Data.Cart
             _context = context;
         }
 
+        public void AddItemToCart(MovieModel movie)
+        {
+            var shoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.ID == movie.ID && n.ShoppingCartID == ShoppingCartID);
+
+            if (shoppingCartItem != null)
+            {
+                shoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartID = ShoppingCartID,
+                    Movie = movie,
+                    Amount = 1
+                };
+
+                _context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+
+            _context.SaveChanges();
+        }
+
         public List<ShoppingCartItem> GetShoppingCartItems ()
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartID == ShoppingCartID).Include(n => n.Movie).ToList());
