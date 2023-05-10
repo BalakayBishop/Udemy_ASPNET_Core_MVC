5d5f68af6ec687fcf9dc53ecc9c6c7e90758b835
Blake Bishop <b.bishop3709@student.nu.edu>
2023-05-10 16:26:32 -0700

    Local: Created Order and OrderItems Model

---
+++ eTickets/eTickets/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Order
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string email { get; set; }
+
+        public string User_ID { get; set; }
+
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ eTickets/eTickets/Models/OrderItem.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eTickets.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int Amount { get; set; }
+
+        public double Price { get; set; }
+
+        // Ref to Movie
+        public int Movie_ID { get; set; }
+        [ForeignKey("Movie_ID")]
+        public MovieModel Movie { get; set; }
+
+        // Ref to Order
+        public int Order_ID { get; set; }
+        [ForeignKey("Order_ID")]
+        public Order Order { get; set; }
+    }
+}
